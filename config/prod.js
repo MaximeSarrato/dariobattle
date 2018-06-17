ec1f4bc52d693ff2ae17add686f67a8bf5e1fd8f
Maxime Sarrato <maxime.sarrato@gmail.com>
2018-06-17 14:15:54 +0200

    Add production env keys

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,7 @@
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET
+	jwtSecret: process.env.JWT_SECRET,
+	mongoURI: process.env.MONGO_URI,
+	saltRounds: process.env.SALT_ROUNDS,
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 };
