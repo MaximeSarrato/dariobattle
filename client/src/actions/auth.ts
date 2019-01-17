c1041366296b0d7a6b2896efb6028c07ac1b5b2a
Maxime Sarrato <maxime.sarrato@gmail.com>
2019-01-17 22:43:05 +0100

    Add type for actions using redux-thunk

--- client/src/actions/auth.ts
+++ client/src/actions/auth.ts
@@ -1,20 +1,23 @@
 import axios from 'axios';
 import { AuthActionTypes } from '../types';
 import { action } from 'typesafe-actions';
+import { Dispatch, Action } from 'redux';
 
 export const login = ({ uid, username }: { uid: string; username: string }) =>
   action(AuthActionTypes.LOGIN, { uid, username });
 
 export const startLogin = (username: string, password: string) => {
-  return dispatch => {
+  return (dispatch: Dispatch<Action<AuthActionTypes.LOGIN>>) => {
     axios
       .post('/users/login', { username, password }, { withCredentials: true })
-      .then(response => {
+      .then((response) => {
         localStorage.setItem('token', response.headers['x-auth']);
-        const { uid, username } = response.data;
-        return dispatch(login({ uid, username }));
+        return dispatch(
+          login({ uid: response.data.uid, username: response.data.username })
+        );
       })
-      .catch(err => {
+      .catch((err) => {
+        // FIXME: Handle error without a console.log
         console.log(err.response.data);
         alert(err.response.data);
       });
@@ -22,36 +25,38 @@ export const startLogin = (username: string, password: string) => {
 };
 
 export const startLoginWithTokenOrSID = (token: string) => {
-  return dispatch => {
+  return (dispatch: Dispatch<Action<AuthActionTypes.LOGIN>>) => {
     axios
       .get('/auth/token', {
         headers: { 'x-auth': token },
         withCredentials: true
       })
-      .then(response => {
+      .then((response) => {
         localStorage.setItem('token', response.headers['x-auth']);
 
         const { uid, username } = response.data;
         return dispatch(login({ uid, username }));
       })
-      .catch(err => {
+      .catch((err) => {
+        // FIXME: Handle error without a console.log
         console.log(err.response.data.error);
       });
   };
 };
 
 export const startLogout = (token: string) => {
-  return dispatch => {
+  return (dispatch: Dispatch<Action<AuthActionTypes.LOGOUT>>) => {
     localStorage.clear();
     axios
       .get('/auth/logout', {
         headers: { 'x-auth': token },
         withCredentials: true
       })
-      .then(response => {
+      .then(() => {
         return dispatch(logout());
       })
-      .catch(err => {
+      .catch((err) => {
+        // FIXME: Handle error without a console.log
         console.log(err.response);
       });
   };
--- client/src/actions/chat.ts
+++ client/src/actions/chat.ts
@@ -1,18 +1,17 @@
 import * as io from 'socket.io-client';
-import { Users } from '../../../sockets/WebSocketServer'
-import { AuthActionTypes, ChatActionTypes } from '../types'
+import { Users } from '../../../sockets/WebSocketServer';
+import { ChatActionTypes } from '../types';
 import { action } from 'typesafe-actions';
+import { Dispatch, Action } from 'redux';
 
 let ioURL = window.document.location.origin;
 if (ioURL.indexOf('9000')) {
-  ioURL = ioURL.replace('9000', '3000')
+  ioURL = ioURL.replace('9000', '3000');
 }
-const socket = io.connect(ioURL)
+const socket = io.connect(ioURL);
 
 export const joinChat = (username: string) => {
-  return dispatch => {
-    console.log('ioURL: ', ioURL);
-
+  return (dispatch: Dispatch<Action<ChatActionTypes.POPULATE_USERS>>) => {
     socket.emit('connection');
     socket.emit('joinChat', username);
     socket.on('getUsers', (users: Users) => {
@@ -21,4 +20,5 @@ export const joinChat = (username: string) => {
   };
 };
 
-export const populateUsers = (users: Users) => action(ChatActionTypes.POPULATE_USERS, {users: Object.keys(users)})
+export const populateUsers = (users: Users) =>
+  action(ChatActionTypes.POPULATE_USERS, { users: Object.keys(users) });
